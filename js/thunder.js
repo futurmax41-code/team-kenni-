75b0e937c5d294caef21671ea7eda4c352bcb016
futurmax41-code <futurmax41@gmail.com>
2026-02-25 18:14:37 +0100

    Enhance thunder effect with sound and jumpscare

--- js/thunder.js
+++ js/thunder.js
@@ -1,13 +1,53 @@
-function thunderFlash() {
-    const flash = document.querySelector(".lightning");
-    flash.style.opacity = "1";
+const thunderSound = new Audio("../assets/thunder.mp3");
+const screamSound = new Audio("../assets/scream.mp3");
 
-    setTimeout(() => {
-        flash.style.opacity = "0";
-    }, 120);
+let kills = 0;
+
+function thunderFlash(){
+  const flash=document.querySelector(".lightning");
+  if(!flash) return;
+
+  flash.style.opacity="1";
+  thunderSound.currentTime=0;
+  thunderSound.volume=0.5;
+  thunderSound.play().catch(()=>{});
+
+  setTimeout(()=>flash.style.opacity="0",180);
 }
 
-// tonnerre aléatoire
-setInterval(() => {
-    thunderFlash();
-}, Math.random() * 4000 + 3000);
+setInterval(()=>thunderFlash(),Math.random()*5000+4000);
+
+setInterval(()=>{
+  if(Math.random()<0.18) showJumpscare();
+},12000);
+
+function showJumpscare(){
+  const img=document.createElement("img");
+  img.src="../assets/skull.png";
+  img.style.position="fixed";
+  img.style.inset="0";
+  img.style.width="100%";
+  img.style.height="100%";
+  img.style.objectFit="contain";
+  img.style.background="black";
+  img.style.zIndex="9999";
+  document.body.appendChild(img);
+
+  screamSound.currentTime=0;
+  screamSound.play().catch(()=>{});
+
+  kills++;
+  updateKills();
+
+  setTimeout(()=>img.remove(),900);
+}
+
+function updateKills(){
+  let counter=document.querySelector(".kill-counter");
+  if(!counter){
+    counter=document.createElement("div");
+    counter.className="kill-counter";
+    document.body.appendChild(counter);
+  }
+  counter.textContent="☠️ Dominations : "+kills;
+}
